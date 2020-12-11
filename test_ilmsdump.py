b5c5e599476e689de2ffc176e5174dec8b617a34
Yu, Li-Yu <afg984@gmail.com>
2020-12-11 21:46:09 +0800

    add get_client helper

--- test_ilmsdump.py
+++ test_ilmsdump.py
@@ -1,14 +1,21 @@
+import contextlib
 import pytest
 import tempfile
 
 import ilmsdump
 
 
-@pytest.mark.asyncio
-async def test_anonymous():
+@contextlib.asynccontextmanager
+async def get_client():
     with tempfile.TemporaryDirectory() as tmpd:
         async with ilmsdump.Client(data_dir=tmpd) as client:
-            course = await client.get_course(46274)
-            assert course.id == 46274
-            assert course.name == '平行程式Parallel Programming'
-            assert course.is_admin is False
+            yield client
+
+
+@pytest.mark.asyncio
+async def test_anonymous():
+    async with get_client() as client:
+        course = await client.get_course(46274)
+        assert course.id == 46274
+        assert course.name == '平行程式Parallel Programming'
+        assert course.is_admin is False
