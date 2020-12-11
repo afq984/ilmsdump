a3672727cddbb9800a21dc234a314c0060fb56e0
Yu, Li-Yu <afg984@gmail.com>
2020-12-12 02:55:26 +0800

    test get_meta

--- tests/test_downloadable.py
+++ tests/test_downloadable.py
@@ -3,3 +3,12 @@ from tests import data
 
 def test_as_id_string():
     assert data.COURSE_74.as_id_string() == 'Course-74'
+
+
+def test_get_meta():
+    assert data.COURSE_46274.get_meta() == {
+        'id': 46274,
+        'serial': '10910CS542200',
+        'name': '平行程式Parallel Programming',
+        'is_admin': False,
+    }
