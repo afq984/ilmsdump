b404f7d8190b5510688809aae49de4b1bee08098
Yu, Li-Yu <afg984@gmail.com>
2020-12-12 06:42:01 +0800

    test cli logout

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -17,3 +17,14 @@ def test_nothing_to_do(tempdir):
     runner = click.testing.CliRunner(mix_stderr=False)
     result = runner.invoke(ilmsdump.main, ['--output-dir', tempdir])
     assert result.stderr == 'Nothing to do\n'
+
+
+def test_logout(tempdir):
+    cred_file = os.path.join(tempdir, 'credentials.txt')
+    open(cred_file, 'w').close()
+
+    runner = click.testing.CliRunner(mix_stderr=False)
+    result = runner.invoke(ilmsdump.main, ['--output-dir', tempdir, '--logout'])
+
+    assert not os.path.exists(cred_file)
+    assert 'Removed' in result.stdout
