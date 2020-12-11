bf66161bcda80a86bcd5ff0377d798d68aeec09a
Yu, Li-Yu <afg984@gmail.com>
2020-12-12 03:00:27 +0800

    test get_attachments

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,3 +1,4 @@
+import lxml.html
 import pytest
 import yarl
 
@@ -16,3 +17,17 @@ def test_flatten_attribute():
     assert ilmsdump.flatten_attribute(3) == 3
     assert ilmsdump.flatten_attribute(yarl.URL('http://example.org')) == 'http://example.org'
     assert ilmsdump.flatten_attribute(data.COURSE_74) == data.COURSE_74.as_id_string()
+
+
+def test_get_attachments():
+    html = lxml.html.fromstring(
+        '''
+        <a href="example.com/a">one</a>
+        <a href="/sys/read_attach.php?id=12345" title="attachment.txt">two</a>
+        <a href="/sys/read_attach.php?id=12345" title="attachment.txt">three</a>
+        '''
+    )
+
+    assert list(ilmsdump.get_attachments(data.COURSE_74, html)) == [
+        ilmsdump.Attachment(id=12345, title='attachment.txt', parent=data.COURSE_74),
+    ]
