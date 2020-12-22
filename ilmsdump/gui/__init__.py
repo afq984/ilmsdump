cd7f25e33b822d12dcdc827ee8a57c446295baab
Yu, Li-Yu <afg984@gmail.com>
2020-12-22 20:28:20 +0800

    remove explicit super(...) parameters

--- ilmsdump/gui/__init__.py
+++ ilmsdump/gui/__init__.py
@@ -53,7 +53,7 @@ class DownloadThread(QThread):
 class Form(QObject):
     def __init__(self, ui_file, parent=None):
         # load ui file
-        super(Form, self).__init__(parent)
+        super().__init__(parent)
         ui_file = QFile(ui_file)
         ui_file.open(QFile.ReadOnly)
         loader = QUiLoader()
